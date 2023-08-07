873664c361f09b6594d9c74151f43cf813c5500c
BrunoLK021 <brunoalencarvieira136@gmail.com>
2023-08-07 19:43:16 -0300

    requirements

--- climatempo/settings.py
+++ climatempo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gn&2x17lla3)f_h!ho!=f7cj@ew-k$jyh%+#iap!hri_**8n$a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["http://127.0.0.1:8000/", "https://climalk.onrender.com"]
 
 
 # Application definition
